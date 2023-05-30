b41e9bb38a52e684a7fe60010365adae1f32821d
cong0212 <95029670+cong0212@users.noreply.github.com>
2023-05-30 10:36:41 +0700

    bai js3

---
+++ js3.js
@@ -0,0 +1,51 @@
+
+
+let a = 21;
+let b = 22;
+
+let c = a + b;
+
+console.log(c)
+
+console.log(`cong: dep zai`)
+
+console.log(a == b)
+
+const x = 5; //hangso
+
+
+console.log('a' + 5 + 4) //cong tu trai sang phai
+
+console.log(x, a, b)
+
+let txt = "congdepzai"
+
+console.log(txt.length)
+
+console.log(`check: a = ${a}`)
+
+console.log(`/cong/${a}`)
+
+
+//obj
+
+let obj = {
+    name: 'cong',
+    age: '21',
+    address: 'Nam Dinh'
+};
+
+let change = 'name'
+
+obj.name = 'hanh'
+
+console.log(`my name is: ` , obj[change])
+
+
+//Array
+
+let Arr = [
+    'cong',
+    'vu',
+    'minh'
+];
